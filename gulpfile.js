819cc4ed575386a8c72c5c66a2b004743b859ee5
Goopher97 <1520396767@qq.com>
2021-04-05 01:31:18 +0800

    修改gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,6 @@ var minifycss = require('gulp-minify-css');
 var htmlmin = require('gulp-html-minifier-terser');
 var htmlclean = require('gulp-htmlclean');
 var terser = require('gulp-terser');
-
-// 压缩css文件
-const minify_css = () => (
-    gulp.src(['./public/**/*.css'])
-        .pipe(minifycss())
-        .pipe(gulp.dest('./public'))
-);
-
-// 压缩html文件
 const minify_html = () => (
     gulp.src(['./public/**/*.html','!./public/{lib,lib/**}'])
         .pipe(htmlclean())
@@ -23,23 +14,23 @@ const minify_html = () => (
         }))
         .pipe(gulp.dest('./public'))
 )
-
-// 压缩js文件
+const minify_css = () => (
+    gulp.src(['./public/**/*.css'])
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'))
+);
 const minify_js = () => (
     gulp.src(['./public/**/*.js', '!./public/**/*.min.js','!./public/{lib,lib/**}'])
         .pipe(terser())
         .pipe(gulp.dest('./public'))
 )
-
 module.exports = {
     minify_html: minify_html,
     minify_css: minify_css,
-    minify_js: minify_js
+    minify_js: minify_js,
 };
-gulp.task('one', gulp.parallel(
+gulp.task('default', gulp.parallel(
     minify_html,
     minify_css,
-    minify_js
+    minify_js,
 ))
-
-gulp.task('default', gulp.series('one'));
